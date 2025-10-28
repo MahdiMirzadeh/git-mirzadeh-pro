a1d55ac510621e3d13f50a5f4023eef46a3fbf85
MahdiMirzadeh <mahdi@mirzadeh.pro>
2025-10-28 11:22:47 +0330

    style.css: add some padding for better reading

--- update.sh
+++ update.sh
@@ -5,8 +5,22 @@ OUT_DIR="$HOME/Code/git-mirzadeh-pro/dist/"
 REPOS="
 $HOME/Code/mirzadeh-pro:My personal website
 $HOME/Code/qbittorrent:My qbittorrent themes
-$HOME/Code/excalocal:Local Excalidraw server with custom handwritten font and advanced instance management
-$HOME/Code/git-mirzadeh-pro:source code for git.mirzadeh.pro
+$HOME/Code/excalocal:Local Excalidraw server
+$HOME/Code/kaveh:A lightweight static site generator written in pure POSIX shell
+$HOME/Code/git-mirzadeh-pro:Source code for git.mirzadeh.pro
+$HOME/Code/app-grid-wizard:A gnome shell extension that automates application folders
+$HOME/Code/getip:Simple shell script to obtain Internal/External IP
+$HOME/Code/dumbsh:Your shell is dumb, make it smarter.
+$HOME/Code/suckless-patches:My patches for http://suckless.org/
+$HOME/Code/slock:Mahdi's build of slock
+$HOME/Code/mksh:Mahdi's build of mksh
+$HOME/Code/neatvi:Mahdi's build of neatvi
+$HOME/Code/dmenu:Mahdi's build of dmenu
+$HOME/Code/dwm:Mahdi's build of dwm
+$HOME/Code/st:Mahdi's build of st
+$HOME/Code/tabbed:Mahdi's build of tabbed
+$HOME/Code/surf:Mahdi's build of surf
+$HOME/Code/merbe:Mahdi's build of herbe
 "
 
 ASSETS="
@@ -45,4 +59,4 @@ while read -r REPO; do
 	cp $ASSETS .
 done
 
-stagit-index `echo "$REPOS" | awk -F':' '{print $1}'` > "$OUT_DIR/index.html"
+stagit-index `echo "$REPOS" | awk -F':' '{print $1}' | sort` > "$OUT_DIR/index.html"
