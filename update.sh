58d5b4e37ac142cd37f12c616c36b71dd6c7fdb5
MahdiMirzadeh <mahdi@mirzadeh.pro>
2025-11-03 11:05:15 +0330

    stagit repos: Copying log.html to index.html

--- update.sh
+++ update.sh
@@ -57,8 +57,8 @@ while read -r REPO; do
 	cd "$DIST"
 	stagit "$REPO_NAME"
 	cp $ASSETS .
+
+	cp "log.html" "index.html"
 done
 
 stagit-index `echo "$REPOS" | awk -F':' '{print $1}' | sort` > "$OUT_DIR/index.html"
-
-#proxychains -q wrangler deploy
